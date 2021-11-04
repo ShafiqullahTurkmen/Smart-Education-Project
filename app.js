d52f13afe504d95a6569cc2213fc4565cdc4683f
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2021-11-04 20:17:47 +0300

    links updated

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
+
 const app = express();
 
+//Template Engine
+app.set("view engine", "ejs");
+
+//Middlewares
+app.use(express.static('public'));
+
+//Routes 
 app.get('/', (req, res) => {
-    res.send('Index Sayfası')
+    res.status(200).render('index', {
+        page_name : 'index'
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.status(200).render('about', {
+        page_name: 'about'
+    });
 })
 
 const port = 3000;
