6227e853b9dc7d7fad095a9f50e03774e7e9f2ed
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2021-11-05 08:44:35 +0300

    Routes and Controller folder Added

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
 const express = require('express');
+const pageRouter = require('./routes/pageRouter');
 
 const app = express();
 
 //Template Engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 //Middlewares
 app.use(express.static('public'));
 
-//Routes 
-app.get('/', (req, res) => {
-    res.status(200).render('index', {
-        page_name : 'index'
-    });
-})
+//Routes
+app.use('/', pageRouter);
 
-app.get('/about', (req, res) => {
-    res.status(200).render('about', {
-        page_name: 'about'
-    });
-})
+
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`App started on port ${port}.`);
-})
+  console.log(`App started on port ${port}.`);
+});
