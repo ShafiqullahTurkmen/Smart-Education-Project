7c03dac1d52f88d700e048beeac46da7d730bf2f
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2021-11-07 23:49:09 +0300

    special contents for users added.

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,7 +27,7 @@ exports.loginUser = async (req, res) => {
                     if (same) {
                         // USER Session
                         req.session.userID = user._id;
-                        res.status(200).redirect('/');
+                        res.status(200).redirect('/users/dashboard');
                     }
                 });
             }
@@ -41,7 +41,15 @@ exports.loginUser = async (req, res) => {
 };
 
 exports.logoutUser = (req, res) => {
-  req.session.destroy(() => {
-    res.redirect('/');
-  });
+    req.session.destroy(() => {
+        res.redirect('/');
+    });
+};
+
+exports.getDashboardPage = async (req, res) => {
+    const user = await User.findOne({_id: req.session.userID});
+    res.status(200).render('dashboard', {
+        page_name: 'dashboard',
+        user,
+    });
 };
