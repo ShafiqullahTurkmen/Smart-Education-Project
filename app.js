deb833dbf554c65408562bef4c74fb77e84747c0
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2021-11-07 21:32:33 +0300

    session added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
 const pageRout = require('./routes/pageRout');
 const courseRout = require('./routes/courseRout');
 const categoryRoute = require('./routes/categoryRoute');
@@ -8,33 +9,48 @@ const userRoute = require('./routes/userRoute');
 const app = express();
 
 //Connect DataBase
-mongoose.connect('mongodb://localhost/smart-education-db')
-.then(() => {
-  console.log('DB connected successfully');
-}).catch((err) => {
-  console.log(err);
-})
+mongoose
+    .connect('mongodb://localhost/smart-education-db')
+    .then(() => {
+        console.log('DB connected successfully');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
 
 //Template Engine
 app.set('view engine', 'ejs');
 
+
+//Global Variable
+global.userIN = null;
+
+
 //Middlewares
 app.use(express.static('public'));
-
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(
+    session({
+        secret: 'my_keyboard_cat',
+        resave: false,
+        saveUninitialized: true,
+    })
+);
 
 //Routes
+app.use('*', (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
 app.use('/', pageRout);
 app.use('/courses', courseRout);
 app.use('/categories', categoryRoute);
 app.use('/users', userRoute);
 
-
-
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`App started on port ${port}.`);
+    console.log(`App started on port ${port}.`);
 });
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,42 +1,41 @@
 const User = require('../models/User');
-const bcrypt  = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 exports.createUser = async (req, res) => {
-  try {
-    const user = await User.create(req.body);
+    try {
+        const user = await User.create(req.body);
 
-    res.status(201).json({
-      status: 'success',
-      user,
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      error,
-    });
-  }
+        res.status(201).json({
+            status: 'success',
+            user,
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            error,
+        });
+    }
 };
 
 exports.loginUser = async (req, res) => {
-  try {     
-    const {email, password} = req.body;
+    try {
+        const { email, password } = req.body;
 
-    
-    await User.findOne({email}, (err, user) => {
-      if (user) {
-        bcrypt.compare(password, user.password, (err, same) => {
-          if (same) {
-            // USER Session 
-            res.status(200).send('You are Logged in')
-          }
+        await User.findOne({ email }, (err, user) => {
+            if (user) {
+                bcrypt.compare(password, user.password, (err, same) => {
+                    if (same) {
+                        // USER Session
+                        req.session.userID = user._id;
+                        res.status(200).redirect('/');
+                    }
+                });
+            }
+        }).clone();
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            error,
         });
-      }
-    }).clone()
-    
-  } catch (error) {
-    res.status(400).json({
-      status: "fail",
-      error,
-    });
-  }
+    }
 };
