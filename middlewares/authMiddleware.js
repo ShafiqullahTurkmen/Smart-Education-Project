e345a95f7d3753f14ba3f12abd66b65ecf4af4f9
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2021-11-08 10:03:01 +0300

    Custom Middlewares added

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,10 @@
+const User = require('../models/User');
+
+module.exports = (req, res, next) => {
+    User.findById(req.session.userID, (err, user) => {
+        if (err || !user) {
+            return res.redirect('/login');
+        }
+        next();
+    });
+};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,12 @@
 const express  = require('express');
 const authController  = require('../controllers/authController');
+const authMiddleware = require('../middlewares/authMiddleware')
 
 const router = express.Router();
 
 router.route('/signUp').post(authController.createUser);
 router.route('/login').post(authController.loginUser);
 router.route('/logout').get(authController.logoutUser);
-router.route('/dashboard').get(authController.getDashboardPage);
+router.route('/dashboard').get(authMiddleware, authController.getDashboardPage);
 
 module.exports = router;
