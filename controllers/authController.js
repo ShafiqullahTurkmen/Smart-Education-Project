0c1087545b4a8b39796b04db06987d804bcc31df
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2021-11-09 10:44:23 +0300

    Create Course as teacher added.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
-const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const User = require('../models/User');
+const Category = require('../models/Category');
 
 exports.createUser = async (req, res) => {
     try {
@@ -45,8 +46,10 @@ exports.logoutUser = (req, res) => {
 
 exports.getDashboardPage = async (req, res) => {
     const user = await User.findOne({_id: req.session.userID});
+    const categories = await Category.find();
     res.status(200).render('dashboard', {
         page_name: 'dashboard',
         user,
+        categories,
     });
 };
