62a6ec0dfdf2432df6acdf6c9654641a5dec681c
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2021-11-10 09:43:47 +0300

    Contact page, nodemailer package, and mail specification added.

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,5 @@
+const nodemailer = require('nodemailer');
+
 exports.getIndexPage = (req, res) => {
   console.log(req.session.userID);
   res.status(200).render('index', {
@@ -21,4 +23,65 @@ exports.getLoginPage  = (req, res) => {
   res.status(200).render('login', {
     page_name: 'login'
   })
+}
+
+exports.getContactPage  = (req, res) => {
+  res.status(200).render('contact', {
+    page_name: 'contact'
+  })
+}
+
+exports.sendEmail = async (req, res) => {
+
+
+  try {
+    
+  const outputMessage = `
+  
+  <h1> Mail Details </h1>
+  <ul>
+    <li> Name: ${req.body.name} </li>
+    <li> Email: ${req.body.email} </li>
+  </ul>
+  <h1> Message </h1>
+  <p> ${req.body.message} </p>  
+  `
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: "alparslan007.aa@gmail.com", // gmail account
+      pass: "kmptrhvccybywnyy", // gmail password
+    },
+    tls: {
+      rejectUnauthorized: false
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Smart Edu Contact Form" <alparslan007.aa@gmail.com>', // sender address
+    to: "shafiqullahturkmen@gmail.com", // list of receivers
+    subject: "Smart Edu Contact Form New Message ✔", // Subject line
+    html: outputMessage, // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+  res.status(200).redirect('/contact'); 
+  } catch (error) {
+    res.json({
+      status: 'fail',
+      error
+    })
+  }
+
+
 }
