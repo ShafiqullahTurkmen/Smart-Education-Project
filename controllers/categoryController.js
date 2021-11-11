db060aee3f07cea7b753baeb86c1bf49bea0c922
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2021-11-11 08:30:42 +0300

    Category Adding and Removing added.

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,14 +3,24 @@ const Category = require('../models/Category');
 exports.createCategory = async (req, res) => {
   try {
     const category = await Category.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      category,
-    });
+    res.status(201).redirect('/users/dashboard');
   } catch (error) {
     res.status(400).json({
       status: 'fail',
       error,
     });
+  }
+};
+
+exports.deleteCategory = async (req, res) => {
+  try {
+      await Category.findByIdAndRemove(req.params.id)
+
+      res.status(200).redirect('/users/dashboard');
+  } catch (error) {
+      res.status(200).json({
+          status: 'fail',
+          error,
+      });
   }
 };
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -4,5 +4,6 @@ const categoryController = require('../controllers/categoryController');
 const router = express.Router();
 
 router.route('/').post(categoryController.createCategory);
+router.route('/:id').delete(categoryController.deleteCategory);
 
 module.exports = router;
